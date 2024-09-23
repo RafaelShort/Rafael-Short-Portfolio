40f649cceea1a89e53e6d85ca58d50cab6cb62ca
RafaelShort <66694000+RafaelShort@users.noreply.github.com>
2024-09-23 19:55:48 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 function toggleMenu() {
     const menu = document.getElementById('menu');
-    menu.classList.toggle('hidden'); // Alterna a visibilidade do menu
+    const nav = menu.parentElement; // Pega o elemento pai do menu
+    nav.classList.toggle('active'); // Alterna a classe 'active' no nav
 }
